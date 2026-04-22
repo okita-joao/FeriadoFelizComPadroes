7edaecdcbfa630d214c311d8394852aae9f5fc6e
João Pedro Navarro Okita <joao.okita07@unifesp.br>
2026-04-21 23:04:16 -0300

    Create EstrategiaCalculoBonusSalarial.java

---
+++ CalculaBonusSalarial/EstrategiaCalculoBonusSalarial.java
@@ -0,0 +1,7 @@
+package Projeto_Biblioteca.CalculaBonusSalarial;
+
+import Projeto_Biblioteca.Funcionario;
+
+public interface EstrategiaCalculoBonusSalarial {
+    public double calculaBonusSalarial(Funcionario f);
+}
