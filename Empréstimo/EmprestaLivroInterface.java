ebf6903ca1c1747202b7b9801070145de0af8672
João Pedro Navarro Okita <joao.okita07@unifesp.br>
2026-04-21 23:07:50 -0300

    Create EmprestaLivroInterface.java

---
+++ Empréstimo/EmprestaLivroInterface.java
@@ -0,0 +1,8 @@
+package Projeto_Biblioteca.Empréstimo;
+
+import Projeto_Biblioteca.Leitor;
+import Projeto_Biblioteca.Livro;
+
+interface EmprestaLivroInterface {
+    public void emprestaLivro(Leitor leitor, Livro livro);
+}
