35d37f2aadfe08d6f4129a91cc8eac32a3531b19
João Pedro Navarro Okita <joao.okita07@unifesp.br>
2026-04-21 23:10:04 -0300

    Create LeitorDigitalInterface.java

---
+++ Leitor_Digital/LeitorDigitalInterface.java
@@ -0,0 +1,9 @@
+package Projeto_Biblioteca.Leitor_Digital;
+
+import Projeto_Biblioteca.LivroDigital;
+
+public interface LeitorDigitalInterface {
+    // Método que vai disponibilizar a leitura do Livro Digital, dependendo do
+    // formato de arquivo de apresentação
+    public void apresentarLivroDigital(LivroDigital livro);
+}
